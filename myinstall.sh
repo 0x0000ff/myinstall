4bdbfa5a9cb81ed83a1f29bc979fd38be6dcc08b
0x0000ff <0x0000ff@acme.co.uk>
2018-12-21 00:16:02 +0000

    edits to myinstall.sh

--- myinstall.sh
+++ myinstall.sh
@@ -1,5 +1,4 @@
 #/bin/sh
-mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-$(date +%Y-%m)
 
 # Disk Partitioning and mount
 lsblk
@@ -14,14 +13,25 @@ curl -O http://mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-$(d
 curl -O http://mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-$(date +%Y.%m).01-x86_64.tar.gz.sig
 curl -O http://mirror.rackspace.com/archlinux/iso/latest/md5sums.txt
 curl -O http://mirror.rackspace.com/archlinux/iso/latest/sha1sums.txt
-gpg --keyserver pgp.mit.edu --keyserver-options auto-key-retrieve --verify archlinux-$(date + %Y.%m).01-x86_64.tar.gz.sig
+gpg --keyserver pgp.mit.edu --keyserver-options auto-key-retrieve --verify archlinux-bootstrap-$(date +%Y.%m).01-x86_64.tar.gz.sig
 md5sum -c md5sums.txt
 sha1sum -c sha1sums.txt
-tar -xzvf archlinux-bootstrap-$(date +%Y-%m).01-x86_64.tar.gz -C /mnt
+sleep 5s
+tar -xzvf archlinux-bootstrap-$(date +%Y.%m).01-x86_64.tar.gz -C /mnt
 cp postinstall.sh /mnt
+mkdir /boot/efi
 
 # Chroot
-arch-chroot /mnt
+#arch-chroot /mnt
+mount --bind /mnt /mnt
+cd /mnt
+cp /etc/resolv.conf /mnt/etc
+mount -t proc /proc proc
+mount --make-rslave --rbind /sys sys
+mount --make-rslave --rbind /dev dev
+mount --make-rslave --rbind /run run    # (assuming /run exists on the system)
+chroot /mnt /bin/bash
+
 
 # Base Config
 ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
@@ -37,8 +47,11 @@ echo 127.0.0.1 localhost >> /etc/hostname
 echo ::1 localhost >> /etc/hostname
 echo 127.0.1.1 $MYHOSTNAME.localdomain $MYHOSTNAME >> /etc/hostname
 
+pacman-key --init
+pacman-key --populate archlinux<Paste>
+
 # Base Packages
-echo -ne '\n' | pacman -S --noconfirm git base-devel elinks efibootmgr bluez wpa_supplicant openvpn connman dialog
+echo -ne '\n' | pacman -S --noconfirm git base-devel elinks efibootmgr bluez wpa_supplicant openvpn connman dialog grub efibootmgr
 
 # Mate Packages 
 echo -ne '\n' | sudo pacman -S --noconfirm mate xorg mate-media mate-power-manager system-config-printer blueman arc-gtk-theme arc-icon-theme mate-utils eom
